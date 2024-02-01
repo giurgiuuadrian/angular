62723a75958721823555098dd745caefc7f0c0f2
giurgiuuadrian <giurgiu.adrian01@gmail.com>
2024-02-01 18:23:00 +0200

    recipe list and detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test recipe',
+      'This is a test recipe',
+      'https://www.budgetbytes.com/wp-content/uploads/2013/07/How-to-Calculate-Recipe-Costs-H.jpg'
+    ),
+    new Recipe(
+      'Test recipe',
+      'This is a test recipe',
+      'https://www.budgetbytes.com/wp-content/uploads/2013/07/How-to-Calculate-Recipe-Costs-H.jpg'
+    ),
+    new Recipe(
+      'Test recipe',
+      'This is a test recipe',
+      'https://www.budgetbytes.com/wp-content/uploads/2013/07/How-to-Calculate-Recipe-Costs-H.jpg'
+    ),
+  ];
 }
